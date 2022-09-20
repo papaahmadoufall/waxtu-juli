785fff55f3c01163a4d2772b4af3e258782cd45d
papa ahmadou fall <pahmadoudou@gmail.com>
2022-09-20 16:47:20 +0000

    fixing api fetch

--- src/App.js
+++ src/App.js
@@ -132,7 +132,7 @@ setNextPrayerTime(prayer)
       <header className="header">
         <nav className="navbar">
           <div className="logo">
-            <img src={logo} alt="" className={logo} />
+            <img src={logo} alt="" className={logo} /> hello
           </div>
           <div className="search">
             <label htmlFor="search">Recherche</label>
